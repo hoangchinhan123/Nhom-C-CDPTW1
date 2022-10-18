968f248dfc8050e33ae4936cefeb2a859fb2b238
nhan <chinhanhoan@gmail.com>
2022-10-18 17:14:23 +0700

    cap nhat container

---
+++ CDPTW1/public/js/js.js
@@ -0,0 +1,39 @@
+$('#hotid').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: '.contron-pre-next',
+    autoplaySpeed: 1000,
+    arrows: true,
+    autoplay: true,
+});
+$('.intem-main').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: '.contron-pre-next',
+    autoplaySpeed: 1000,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+});
+$('.cont-bottum-main').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    appendArrows: '.contron-pre-next',
+    autoplaySpeed: 1000,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+});
+$('.intem-thume').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    appendArrows: '.contron-pre-next',
+    autoplaySpeed: 1000,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+});
